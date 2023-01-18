29d5c6456182e99037323e6ea5dad065e9c3399a
Raihan Ahmed Dip <raihan.dip@gmail.com>
2023-01-19 00:41:09 +0600

    Saved the vectorizer

---
+++ NLP/Spam_SMS_Detection/real_time_sms_detection.py
@@ -0,0 +1,21 @@
+import pickle
+
+def classify_sms(sms):
+    # Load the saved model
+    with open('spam_classifier.pkl', 'rb') as f:
+        clf = pickle.load(f)
+
+    # Vectorize the input SMS
+    sms_vector = vectorizer.transform([sms])
+    
+    # Predict the class
+    label = clf.predict(sms_vector)
+
+    if label[0] == 0:
+        return "Not Spam"
+    else:
+        return "Spam"
+
+# Example usage
+new_sms = input("Write the SMS:")
+print(classify_sms(new_sms))  # Output: Spam
